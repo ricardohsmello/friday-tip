29b38539d8329c93b899162b8fc84f0ebafb257d
Ricardo <ricardohsmello@gmail.com>
2024-12-06 21:47:43 -0300

    creating Tip Controller

---
+++ src/main/java/br/com/ricas/TipController.java
@@ -0,0 +1,13 @@
+package br.com.ricas;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tips")
+public class TipController {
+
+    private final TipService tipService;
+
+    public
+}
