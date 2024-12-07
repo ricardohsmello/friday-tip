392379501a9f9732849449d314492d862a527124
Ricardo <ricardohsmello@gmail.com>
2024-12-06 21:47:23 -0300

    creating MongoConfig

---
+++ src/main/java/br/com/ricas/MongoConfig.java
@@ -0,0 +1,28 @@
+package br.com.ricas;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ReadPreference;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MongoConfig {
+
+    @Bean
+    public MongoClient mongoClient() {
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .readPreference(ReadPreference.nearest())
+                .build();
+
+        return MongoClients.create(settings);
+    }
+
+    @Value("${spring.data.mongodb.uri}")
+    private String connectionString;
+
+}
