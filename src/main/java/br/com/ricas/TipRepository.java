f18a08e11b9b524acbad7b6fb55d2fe83b163c5c
Ricardo <ricardohsmello@gmail.com>
2024-12-06 22:10:25 -0300

    Refactoring

--- src/main/java/br/com/ricas/TipRepository.java
+++ src/main/java/br/com/ricas/TipRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TipRepository extends MongoRepository<Tip, String> {
+public interface TipRepository extends MongoRepository<FridayTip, String> {
 
 }
