63673f8da3dbdb7462daf451a1b7695152bf3752
Ricardo <ricardohsmello@gmail.com>
2024-12-06 22:09:09 -0300

    creating Tip Controller

--- src/main/java/br/com/ricas/TipController.java
+++ src/main/java/br/com/ricas/TipController.java
@@ -1,13 +1,28 @@
 package br.com.ricas;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/tips")
 public class TipController {
 
+    Logger logger = Logger.getLogger("TipController");
+
     private final TipService tipService;
 
-    public
+    public TipController(TipService tipService) {
+        this.tipService = tipService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Tip> addTip(@RequestBody Tip tip) {
+        logger.info("Creating a new tip" + tip);
+        return ResponseEntity.ok(tipService.save(tip));
+    }
 }
