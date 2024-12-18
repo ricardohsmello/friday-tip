9c83ffe8f4b8db9bc77275d66950ae8edaa27b8f
Ricardo <ricardohsmello@gmail.com>
2024-12-17 23:31:40 -0300

    Refactoring

--- src/main/java/br/com/ricas/transaction/Transaction.java
+++ src/main/java/br/com/ricas/transaction/Transaction.java
@@ -1,9 +1,14 @@
 package br.com.ricas.transaction;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "transaction")
 public class Transaction {
+
+    @Id
+    private String id;
+    private String code;
 
 
 }
