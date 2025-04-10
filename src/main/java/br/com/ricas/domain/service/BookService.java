071fe8ebbb6b69773c3660ac3dd32077869c6bea
Ricardo <ricardohsmello@gmail.com>
2025-04-10 18:34:07 -0300

    Add pagination and enhance MongoDB configuration
    Introduce default pagination to `findAll` in `BookService` and add a `MongoTemplate` bean for MongoDB operations in `MongoConfig`. These changes improve data retrieval flexibility and facilitate database interactions.

--- src/main/java/br/com/ricas/domain/service/BookService.java
+++ src/main/java/br/com/ricas/domain/service/BookService.java
@@ -2,12 +2,18 @@ package br.com.ricas.domain.service;
 
 import br.com.ricas.domain.model.Book;
 import br.com.ricas.domain.repository.BookRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class BookService {
+
+    public static final int DEFAULT_PAGE_NUMBER = 0;
+
+    public static final int DEFAULT_PAGE_SIZE = 10;
 
     private final BookRepository bookRepository;
 
@@ -20,6 +26,16 @@ public class BookService {
     }
 
     public List<Book> findAll() {
-        return bookRepository.findAll();
+        PageRequest request = PageRequest.of(DEFAULT_PAGE_NUMBER, DEFAULT_PAGE_SIZE, Sort.unsorted());
+
+        return bookRepository.findAll(request);
+    }
+
+    public List<Book> fullTextSearch(String therm) {
+        return bookRepository.fullTextSearch(therm);
+    }
+
+    public void groupBooksByAuthorAndExport() {
+        bookRepository.groupBooksByAuthorAndExport();
     }
 }
--- src/main/java/br/com/ricas/infrastructure/config/MongoConfig.java
+++ src/main/java/br/com/ricas/infrastructure/config/MongoConfig.java
@@ -6,9 +6,17 @@ import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 public class MongoConfig {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String connectionString;
+
+    @Value("${spring.data.mongodb.database}")
+    private String database;
 
     @Bean
     public MongoClient mongoClient() {
@@ -24,7 +32,9 @@ public class MongoConfig {
         return MongoClients.create(settings);
     }
 
-    @Value("${spring.data.mongodb.uri}")
-    private String connectionString;
+    @Bean
+    public MongoOperations mongoTemplate(MongoClient mongoClient) {
+        return new MongoTemplate(mongoClient, database);
+    }
 
 }
