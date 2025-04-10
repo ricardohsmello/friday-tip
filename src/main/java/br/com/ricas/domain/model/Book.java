a8b2acde07cd4ca68e05d77d74c8405db967a547
Ricardo <ricardohsmello@gmail.com>
2025-04-10 18:32:17 -0300

    Add more fields to Book model and update conversion logic
    Enhanced the Book record by adding id, pages, year, genres, synopsis, and cover fields to better represent book data. Updated the toDocument method to handle the additional fields for seamless data mapping.

--- src/main/java/br/com/ricas/domain/model/Book.java
+++ src/main/java/br/com/ricas/domain/model/Book.java
@@ -2,9 +2,23 @@ package br.com.ricas.domain.model;
 
 import br.com.ricas.infrastructure.database.mongodb.document.BookDocument;
 
-public record Book(String title, String author) {
+import java.util.List;
+
+public record Book(
+        String id,
+        String title,
+        String author,
+        int pages,
+        int year,
+        List<String> genres,
+        String synopsis,
+        String cover) {
+
+    public static String BOOK_FIELD_TITLE = "title";
+    public static String BOOK_FIELD_AUTHOR = "author";
+    public static String BOOK_COLLECTION_NAME = "books";
 
     public BookDocument toDocument() {
-        return new BookDocument(null, this.title, this.author);
+        return new BookDocument(null, this.title, this.author, this.pages, this.year, this.genres, this.synopsis, this.cover);
     }
 }
