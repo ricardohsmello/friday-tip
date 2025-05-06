c0c53ef88c8e9742fd210bd5fc89d98fde431d48
Ricardo <ricardohsmello@gmail.com>
2025-05-06 13:53:58 -0300

    including pom

---
+++ src/test/java/br/com/ricas/FridayFridayTipApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.ricas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridayFridayTipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+//
