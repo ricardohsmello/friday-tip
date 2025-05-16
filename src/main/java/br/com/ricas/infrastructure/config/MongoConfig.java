d760d4a38544dd3e934606a593263f0ae4da8341
Ricardo <ricardohsmello@gmail.com>
2025-05-16 18:10:03 -0300

    Including mappingMongoConverter to remove _class field during insertion

--- src/main/java/br/com/ricas/infrastructure/config/MongoConfig.java
+++ src/main/java/br/com/ricas/infrastructure/config/MongoConfig.java
@@ -3,20 +3,31 @@ package br.com.ricas.infrastructure.config;
 import com.mongodb.*;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Collections;
 
 @Configuration
-public class MongoConfig {
+@EnableTransactionManagement(proxyTargetClass = true)
 
+public class MongoConfig {
+    /*
     @Value("${spring.data.mongodb.uri}")
     private String connectionString;
 
     @Value("${spring.data.mongodb.database}")
     private String database;
+
 
     @Bean
     public MongoClient mongoClient() {
@@ -31,10 +42,15 @@ public class MongoConfig {
 
         return MongoClients.create(settings);
     }
+    */
 
-    @Bean
-    public MongoOperations mongoTemplate(MongoClient mongoClient) {
-        return new MongoTemplate(mongoClient, database);
-    }
+  @Bean
+  public MongoTemplate mongoTemplate(
+          MongoDatabaseFactory databaseFactory,
+          MappingMongoConverter converter
+  ) {
+    converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+    return new MongoTemplate(databaseFactory, converter);
+  }
 
 }
